b4355de3726bb96cc529e952d41185a3aea8c11f
Administrator <thespherret@gmail.com>
2014-11-08 20:34:28 -0500

    Help

--- src/me/pauzen/forceopblocker/Main.java
+++ src/me/pauzen/forceopblocker/Main.java
@@ -1,8 +1,11 @@
 package me.pauzen.forceopblocker;
 
+import me.pauzen.jlib.reflection.Reflection;
+import net.minecraft.server.v1_7_R4.OpList;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_7_R4.CraftServer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,12 +13,18 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        //Objects.setClass(MinecraftServer.getServer().getPlayerList(), PlayerListV2.class);
         /*if (Bukkit.getPluginManager().getPlugin("JLib Plugin") == null) {
             Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Cannot find JLib plugin. Disabling.");
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }*/
         Injector.inject();
+        OpList opList = ((CraftServer) Bukkit.getServer()).getHandle().getOPs();
+        Reflection<OpList> opListReflection = new Reflection<>(opList);
+        System.out.println(opListReflection.callMethod("d", new Object()));
+        System.out.println(opList.getClass());
     }
 
     @Override
@@ -28,4 +37,15 @@ public final class Main extends JavaPlugin {
             }
         return true;
     }
+
+    /*public class PlayerListV2 extends PlayerList {
+
+        public PlayerListV2(MinecraftServer minecraftserver) {
+            super(minecraftserver);
+        }
+
+        @Override
+        public void changeDimension(EntityPlayer entityplayer, int i, org.bukkit.event.player.PlayerTeleportEvent.TeleportCause cause) {
+        }
+    }*/
 }
